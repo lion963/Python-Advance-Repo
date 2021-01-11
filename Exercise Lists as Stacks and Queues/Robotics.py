7474ddf07f69ac9d0e54f202183f6a7c4271900d
Asen Kamboshev <lion963@mail.bg>
2021-01-11 11:26:10 +0200

    tasks 11.01

--- Exercise Lists as Stacks and Queues/Robotics.py
+++ Exercise Lists as Stacks and Queues/Robotics.py
@@ -7,7 +7,8 @@ minutes = int(minutes)
 seconds = int(seconds)
 robots_time = {}
 products = deque()
-second_count=0
+second_count = 0
+round = 0
 
 for el in robots_list:
     robot, process_time = el.split('-')
@@ -21,16 +22,39 @@ while True:
     products.append(product)
 
 while products:
-    second_count+=1
-    fist_product=products[0]
-    for robot, value in robots_time.items():
-        if value[0] == 'available':
-            print(f'{robot} - {products.popleft()} [time]')
-            value[0]='busy'
-        if value[1]%second_count==0:
-            value[0]='available'
-    if fist_product==products[0]:
-        products.append(fist_product)
-        products.popleft()
+    fist_product = products[0]
+    round += 1
+    for (robot, time) in robots_time.items():
+        second_count += 1
+        seconds += 1
+        if seconds == 60:
+            seconds = 0
+            minutes += 1
+            if minutes == 60:
+                hours += 1
+                if hours == 23 and minutes == 60:
+                    hours = 0
+                minutes = 0
+        if not time[0] == 'available':
+            if time[0] % time[1] == 0:
+                time[0] = 'available'
+        if time[0] == 'available':
+            if hours < 10:
+                hours_str = '0' + str(hours)
+            else:
+                hours_str = str(hours)
+            if minutes < 10:
+                minutes_str = '0' + str(minutes)
+            else:
+                minutes_str = str(minutes)
+            if seconds < 10:
+                seconds_str = '0' + str(seconds)
+            else:
+                seconds_str = str(seconds)
+            print(f'{robot} - {products.popleft()} [{hours_str}:{minutes_str}:{seconds_str}]')
+        time[0] = second_count
 
-
+    if products:
+        if fist_product == products[0]:
+            products.append(fist_product)
+            products.popleft()
