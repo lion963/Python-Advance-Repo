24ade30027b257862b187f1a3cb121c56064e167
Asen Kamboshev <lion963@mail.bg>
2021-01-20 14:22:27 +0200

    tasks 20.01

---
+++ Functions Advanced/Absolute Values.py
@@ -0,0 +1,3 @@
+abs_list = list(map(float, input().split()))
+abs_list = [abs(num) for num in abs_list]
+print(abs_list)
---
+++ Functions Advanced/Rounding.py
@@ -0,0 +1,3 @@
+round_list = list(map(float, input().split()))
+round_list = [round(num) for num in round_list]
+print(round_list)
