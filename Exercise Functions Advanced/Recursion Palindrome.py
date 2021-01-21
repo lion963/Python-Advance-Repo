dde6f9b88bf7427038fba531d6610dc026c0f8d5
Asen Kamboshev <lion963@mail.bg>
2021-01-21 13:57:08 +0200

    tasks 21.01

---
+++ Exercise Functions Advanced/Recursion Palindrome.py
@@ -0,0 +1,12 @@
+def palindrome(word, index):
+    if word[index] != word[-index - 1]:
+        return f'{word} is not a palindrome'
+    if len(word) == 1:
+        return f'{word} is a palindrome'
+    if index == len(word) // 2 - 1:
+        return f'{word} is a palindrome'
+    return palindrome(word, index + 1)
+
+
+print(palindrome("abcba", 0))
+print(palindrome("peter", 0))
