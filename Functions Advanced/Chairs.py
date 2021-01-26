02f52a215f84125e743458134bebaa7a4919c0bb
Asen Kamboshev <lion963@mail.bg>
2021-01-26 08:41:47 +0200

    tasks 26.01

--- Functions Advanced/Chairs.py
+++ Functions Advanced/Chairs.py
@@ -1,3 +1,21 @@
+def calc_combinations(names, n, combs=[]):
+    if len(combs) == n:
+        print(", ".join(combs))
+        return
+
+    for i in range(len(names)):
+        name = names[i]
+        combs.append(name)
+        calc_combinations(names[i+1:], n, combs)
+        combs.pop()
+
+
+names = input().split(", ")
+n = int(input())
+
+calc_combinations(names, n)
+
+
 # from itertools import combinations
 # name_list=input().split(', ')
 # n=int(input())
@@ -5,23 +23,24 @@
 # for el in comb:
 #     print(', '.join(el))
 
-def combinations(list, n, combos=[]):
-    if combos is None:
-        combos = []
-    if len(list) == n:
-        if combos.count(list) == 0:
-            combos.append(list)
-            print(', '.join(list))
-            # combos.sort()
-        return combos
-    else:
-        for i in range(len(list)):
-            refined_list = list[:i] + list[i + 1:]
-            combos = combinations(refined_list, n, combos)
-        return combos
+# def combinations(list, n, combos=[]):
+#     if combos is None:
+#         combos = []
+#     if len(list) == n:
+#         if combos.count(list) == 0:
+#             combos.append(list)
+#             print(', '.join(list))
+#             # combos.sort()
+#         return combos
+#     else:
+#         for i in range(len(list)):
+#             refined_list = list[:i] + list[i + 1:]
+#             combos = combinations(refined_list, n, combos)
+#         return combos
+#
+#
+# name_list = input().split(', ')
+# n = int(input())
+#
+# combinations(name_list, n)
 
-
-name_list = input().split(', ')
-n = int(input())
-
-combinations(name_list, n)
