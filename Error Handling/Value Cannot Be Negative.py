89c440348749c6864c7edc05b6521a181264300d
Asen Kamboshev <lion963@mail.bg>
2021-02-01 15:49:05 +0200

    tasks 01.02

--- Error Handling/Value Cannot Be Negative.py
+++ Error Handling/Value Cannot Be Negative.py
@@ -2,7 +2,7 @@ class ValueCannotBeNegative(Exception):
     """Number is below zero"""
     pass
 
-    for i in range(5):
-        number = int(input())
-        if number < 0:
-            raise ValueCannotBeNegative
+for i in range(5):
+    number = int(input())
+    if number < 0:
+        raise ValueCannotBeNegative
