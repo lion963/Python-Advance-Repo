52349c408e06d770f7f1ff1c1c7a8e191df905d9
Asen Kamboshev <lion963@mail.bg>
2021-02-03 13:10:42 +0200

    tasks 03.02

---
+++ Exercise_Modules/gui_shop_live_demo/main.py
@@ -0,0 +1,7 @@
+from Exercise_Modules.gui_shop_live_demo.canvas import tk
+from Exercise_Modules.gui_shop_live_demo.authentication import render_main_enter_screen
+
+
+if __name__ == "__main__":
+    render_main_enter_screen()
+    tk.mainloop()
