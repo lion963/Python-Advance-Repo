66db7551f8fa2896d3b1fdc1ce77b3a6e4559f54
Asen Kamboshev <lion963@mail.bg>
2021-02-15 14:09:11 +0200

    optimisation_exam_14_02 15.02

--- exam_14_02_2021/problem_2.py
+++ exam_14_02_2021/problem_2.py
@@ -3,25 +3,37 @@ import math
 
 def create_matrix(rows, columns):
     matrix = []
+    player_position = None
     for i in range(rows):
         matrix.append([])
         matrix[i] += [int(symbol) if symbol.isdigit() else symbol for symbol in input().split()]
-    return matrix
-
-
-def player_position(matrix):
-    position_p = None
-    for i in range(len(matrix)):
         if 'P' in matrix[i]:
-            position_p = [i, matrix[i].index('P')]
-            break
-    return position_p
+            player_position = [i, matrix[i].index('P')]
+    return matrix, player_position
 
 
-def up(matrix, position, coins, is_wall, is_out):
-    row_index = position[0] - 1
-    col_index = position[1]
-    if 0 <= row_index < len(matrix) and 0 <= col_index < len(matrix):
+def validate(row, col, matrix):
+    return 0 <= row < len(matrix) and 0 <= col < len(matrix)
+
+
+def check_wall_out(wall, out):
+    return wall or out
+
+
+def check_coins(coins):
+    if coins >= 100:
+        return True
+    return False
+
+
+def moves(matrix, command, position, coins, is_wall, is_out):
+    deltas = {"up": (-1, 0),
+              "right": (0, +1),
+              "down": (+1, 0),
+              "left": (0, -1)}
+    row_index = position[0] + deltas[command][0]
+    col_index = position[1] + deltas[command][1]
+    if validate(row_index, col_index, matrix):
         if not matrix[row_index][col_index] == 'X':
             coins += matrix[row_index][col_index]
             position = [row_index, col_index]
@@ -34,91 +46,38 @@ def up(matrix, position, coins, is_wall, is_out):
     return matrix, position, coins, is_wall, is_out
 
 
-def right(matrix, position, coins, is_wall, is_out):
-    row_index = position[0]
-    col_index = position[1] + 1
-    if 0 <= row_index < len(matrix) and 0 <= col_index < len(matrix):
-        if not matrix[row_index][col_index] == 'X':
-            coins += matrix[row_index][col_index]
-            position = [row_index, col_index]
-            matrix[position[0]][position[1]] = 0
-            matrix[row_index][col_index] = 'P'
-        else:
-            is_wall = True
+def print_result(wall, out, path, coins):
+    if wall or out:
+        coins = math.floor(coins / 2)
+        print(f"Game over! You've collected {coins} coins.")
+        print(f"Your path:")
+        [print(pos) for pos in path]
     else:
-        is_out = True
-    return matrix, position, coins, is_wall, is_out
-
-
-def down(matrix, position, coins, is_wall, is_out):
-    row_index = position[0] + 1
-    col_index = position[1]
-    if 0 <= row_index < len(matrix) and 0 <= col_index < len(matrix):
-        if not matrix[row_index][col_index] == 'X':
-            coins += matrix[row_index][col_index]
-            position = [row_index, col_index]
-            matrix[position[0]][position[1]] = 0
-            matrix[row_index][col_index] = 'P'
-        else:
-            is_wall = True
-    else:
-        is_out = True
-    return matrix, position, coins, is_wall, is_out
-
-
-def left(matrix, position, coins, is_wall, is_out):
-    row_index = position[0]
-    col_index = position[1] - 1
-    if 0 <= row_index < len(matrix) and 0 <= col_index < len(matrix):
-        if not matrix[row_index][col_index] == 'X':
-            coins += matrix[row_index][col_index]
-            position = [row_index, col_index]
-            matrix[position[0]][position[1]] = 0
-            matrix[row_index][col_index] = 'P'
-        else:
-            is_wall = True
-    else:
-        is_out = True
-    return matrix, position, coins, is_wall, is_out
+        print(f"You won! You've collected {coins} coins.")
+        print(f"Your path:")
+        [print(pos) for pos in path]
 
 
 n = int(input())
 rows = n
 columns = n
 
-board = create_matrix(rows, columns)
-position_player = player_position(board)
+board, position_player = create_matrix(rows, columns)
 is_wall = False
 is_out = False
 path = []
 coins = 0
 
-
 while True:
     command = input()
-    if not command:
-        break
-    if command == 'up':
-        board, position_player, coins, is_wall, is_out = up(board, position_player, coins, is_wall, is_out)
-    elif command == 'right':
-        board, position_player, coins, is_wall, is_out = right(board, position_player, coins, is_wall, is_out)
-    elif command == 'down':
-        board, position_player, coins, is_wall, is_out = down(board, position_player, coins, is_wall, is_out)
-    elif command == 'left':
-        board, position_player, coins, is_wall, is_out = left(board, position_player, coins, is_wall, is_out)
+    board, position_player, coins, is_wall, is_out = moves(board, command, position_player, coins, is_wall, is_out)
 
-    if is_wall or is_out:
+    if check_wall_out(is_wall, is_out):
         break
+
     path.append(position_player)
-    if coins >= 100:
+
+    if check_coins(coins):
         break
 
-if is_wall or is_out:
-    coins = math.floor(coins / 2)
-    print(f"Game over! You've collected {coins} coins.")
-    print(f"Your path:")
-    [print(pos) for pos in path]
-else:
-    print(f"You won! You've collected {coins} coins.")
-    print(f"Your path:")
-    [print(pos) for pos in path]
+print_result(is_wall, is_out, path, coins)
--- exam_14_02_2021/problem_3.py
+++ exam_14_02_2021/problem_3.py
@@ -1,6 +1,6 @@
 def stock_availability(delivery_list, *args):
     if args[0] == 'delivery':
-        if len(args)>1:
+        if len(args) > 1:
             for box in args[1:]:
                 delivery_list.append(box)
 
@@ -8,13 +8,13 @@ def stock_availability(delivery_list, *args):
         if len(args) == 1:
             if delivery_list:
                 delivery_list.pop(0)
-        elif len(args)>1:
+        elif len(args) > 1:
             for el in args[1:]:
-                if type(el)==int:
+                if type(el) == int:
                     if delivery_list:
                         for i in range(el):
                             delivery_list.pop(0)
-                elif type(el)==str:
+                elif type(el) == str:
                     while el in delivery_list:
                         delivery_list.remove(el)
 
